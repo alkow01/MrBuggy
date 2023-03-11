ab836dbf66542c45e0bffe71f8a9a3aaf53377bf
alkow01 <alicja01kowalska@gmail.com>
2023-03-11 19:19:41 +0100

    Add cypress configuration file

---
+++ cypress.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    'baseUrl': "http://demo-sii.mrbuggy2.testarena.pl/",
+    "includeShadowDom": true,
+    "chromeWebSecurity": true,
+    "viewportHeight": 1080,
+    "viewportWidth": 1920,
+
+}
+});
+
