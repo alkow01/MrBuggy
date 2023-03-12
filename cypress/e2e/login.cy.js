c301b908773022621e9990df2c75919c80c62487
alkow01 <alicja01kowalska@gmail.com>
2023-03-12 17:33:16 +0100

    Added BeforeEach to Login tests

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,6 +1,9 @@
-describe('Login Test', () =>{
+describe('Login Test', () => {
+    beforeEach( () => {
+        cy.basicAuthLogin()
+    })
+
     it('Successful login by providing correct e-mail and password', () => {
-      cy.basicAuthLogin()
       cy.get('#email').should('be.visible')
       cy.get('#password').should('be.visible')
       cy.login('bdb31@waitloek.fun', 'EX7fO2x8')
@@ -8,25 +11,21 @@ describe('Login Test', () =>{
       })
       
     it('Fail login for provided incorrect e-mail and password', () => {
-      cy.basicAuthLogin()
       cy.login('test@test.pl', 'test')
       cy.get('div.login_form_error').contains('Adres e-mail i/lub hasło są niepoprawne.').should('be.visible')
     })
 
     it('Fail login for provided incorrect e-mail and correct password', () => {
-      cy.basicAuthLogin()
       cy.login('test@test.pl', 'EX7fO2x8')
       cy.get('div.login_form_error').contains('Adres e-mail i/lub hasło są niepoprawne.').should('be.visible')
       })
 
     it('Fail login for provided correct e-mail and incorrect password', () => {
-      cy.basicAuthLogin()
       cy.login('bdb31@waitloek.fun', 'test1')
       cy.get('div.login_form_error').contains('Adres e-mail i/lub hasło są niepoprawne.').should('be.visible')
     })
 
     it('Fail login for provided incorrect e-mail and empty field for password', () => {
-      cy.basicAuthLogin()
       cy.login('bdb31@waitloek.fun')
       cy.get('input#login').click()
       cy.get('div.login_form_error').contains('Adres e-mail i/lub hasło są niepoprawne.').should('be.visible')
@@ -41,7 +40,6 @@ describe('Login Test', () =>{
     })
 
     it('Fail login for provided empty e-mail and password fields', () => {
-      cy.basicAuthLogin()
       cy.login('', '')
       cy.get('#login').click()
       cy.get('div.login_form_error').contains('Adres e-mail i/lub hasło są niepoprawne.').should('be.visible')
