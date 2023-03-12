66bff15612525df5cda5845a153a945d33b8f131
alkow01 <alicja01kowalska@gmail.com>
2023-03-12 22:31:15 +0100

    Added cy.reload() to remember_me_checkbox test

--- cypress/e2e/remember_me_checkbox.cy.js
+++ cypress/e2e/remember_me_checkbox.cy.js
@@ -3,6 +3,7 @@ describe('"Remember me" test', () => {
       cy.basicAuthLogin()
       cy.get('#remember').check().should('be.visible')
       cy.login(Cypress.env('active_user_username'), Cypress.env('active_user_password'))
+      cy.reload()
       cy.basicAuthLogin()
       cy.get('[href="http://demo-sii.mrbuggy2.testarena.pl/wyloguj"]').click().should("be.visible")
       cy.get('#login').should('be.visible')
